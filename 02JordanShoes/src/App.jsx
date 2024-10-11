bbbae6c05c245f2c440c10652780c168f8e06e29
Erick Araújo <araujoerick.dev@gmail.com>
2024-10-11 19:41:10 -0300

    :sparkles: feat: Cria Home

--- 02JordanShoes/src/App.jsx
+++ 02JordanShoes/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         <p className="text-center text-2xl p-2">
           Frete grátis para todo o Brasil
         </p>
-        <div className="bg-[url('./src/assets/image-michael-jordan.png')] text-white py-20">
+        <div className="bg-[url('./src/assets/image-michael-jordan.png')] text-white py-24">
           <div className="flex flex-col gap-8 m-auto max-w-screen-xl px-8">
             <div className="flex items-center">
               <img src="./src/assets/logo-jordan.svg" alt="" />
@@ -47,30 +47,38 @@ function App() {
         <p>Carregando...</p>
       ) : (
         <>
-          <main
-            className="m-auto max-w-screen-xl px-8"
-            style={{ display: 'flex', flexWrap: 'wrap' }}
-          >
-            {tenis.map(tenisItem => (
-              <div
-                key={tenisItem.id}
-                style={{
-                  margin: '20px',
-                  width: '200px',
-                  border: '1px solid #ccc',
-                  padding: '10px',
-                }}
-              >
-                <img
-                  src={tenisItem.imagem}
-                  alt={tenisItem.nome}
-                  style={{ width: '100%' }}
-                />
-                <h3>{tenisItem.nome}</h3>
-                <p>Categoria: {tenisItem.categoria}</p>
-                <p>Preço: R$ {tenisItem.preco}</p>
-              </div>
-            ))}
+          <main className="mt-20 m-auto max-w-screen-xl px-8 flex flex-col gap-8">
+            <div className="text-center flex flex-col gap-6 m-auto">
+              <h1 className="text-[2rem] font-semibold">
+                Os melhores em só lugar
+              </h1>
+              <p className="text-2xl max-w-[44ch]">
+                A marca Jordan na JordanShoes é a escolha certa para os amantes
+                de sneakers que buscam estilo e conforto.
+              </p>
+            </div>
+            <div className="flex flex-1 justify-center flex-wrap gap-8 mb-14">
+              {tenis.map(tenisItem => (
+                <div key={tenisItem.id} className="w-96">
+                  <div className="bg-[#F3F7FF] px-20">
+                    <img
+                      src={tenisItem.imagem}
+                      alt={tenisItem.nome}
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <h3 className="text-lg font-medium">{tenisItem.nome}</h3>
+                    <p className="font-medium text-[#7CA2F4]">
+                      {tenisItem.categoria}
+                    </p>
+                    <p className="mt-3 text-xl font-medium">
+                      R$ {tenisItem.preco}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </main>
         </>
       )}
