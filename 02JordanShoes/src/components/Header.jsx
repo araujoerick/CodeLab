f3137625663e20ac90c6d493bf55943edb0e2195
Erick Araújo <araujoerick.dev@gmail.com>
2024-10-13 01:27:55 -0300

    :bug: fix: Importação da Hero Image

--- 02JordanShoes/src/components/Header.jsx
+++ 02JordanShoes/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroImage from '../assets/image-michael-jordan.png'
 
 export const Header = () => {
   return (
@@ -6,20 +7,25 @@ export const Header = () => {
       <p className="text-center md:text-2xl sm:text-base text-sm p-2">
         Frete grátis para todo o Brasil
       </p>
-      <div className="hero-image text-white">
-        <div className="flex flex-col gap-8 m-auto max-w-screen-xl px-8 py-24">
-          <div className="flex items-center">
-            <img src="./src/assets/logo-jordan.svg" alt="" />
-            <p className="md:text-2xl font-medium text-lg">JordanShoes</p>
-          </div>
-          <div>
-            <h2 className="md:text-[2rem] mb-4 text-2xl">
-              A melhor loja de Jordan
-            </h2>
-            <p className="md:text-2xl max-w-[39ch] text-lg">
-              O tênis Jordan é fruto de uma velha e forte parceria entre a Nike
-              e o jogador Michael Jordan.
-            </p>
+      <div className="text-white">
+        <div
+          className="hero-image"
+          style={{ backgroundImage: `url(${HeroImage})` }}
+        >
+          <div className="flex flex-col gap-8 m-auto max-w-screen-xl px-8 py-24">
+            <div className="flex items-center">
+              <img src="./src/assets/logo-jordan.svg" alt="" />
+              <p className="md:text-2xl font-medium text-lg">JordanShoes</p>
+            </div>
+            <div>
+              <h2 className="md:text-[2rem] mb-4 text-2xl">
+                A melhor loja de Jordan
+              </h2>
+              <p className="md:text-2xl max-w-[39ch] text-lg">
+                O tênis Jordan é fruto de uma velha e forte parceria entre a
+                Nike e o jogador Michael Jordan.
+              </p>
+            </div>
           </div>
         </div>
       </div>
